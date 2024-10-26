02690adeaa6465052dda47fc887a9f5680a0c04d
Anmol-Kumar01 <anmol888236@gmail.com>
2024-10-26 23:37:35 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Widget_Habit_Tracker/', // Replace <repo-name> with the actual repository name
 })
